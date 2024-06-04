6541e2d6bf3f2f7c055452dc198980bb54b84b45
puraviasirini <puraviasirini123@gmail.com>
2024-06-04 22:48:00 +0530

    Initial commit - Created .NET Core Web API project and added .gitignore

---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentApp.API.Models
+{
+    public class PaymentDetailContext :DbContext
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+}
